68395061c626316ca968db3a04efa529c3ec0de6
andrija-tosic <andonnet@gmail.com>
2022-12-06 23:26:51 +0100

    excess cleanup

--- lab5.py
+++ lab5.py
@@ -2,33 +2,17 @@
 # heuristikom za raspoređivanje kraljica na šahovskoj tabli dimenzije 8x8 tako da se ne napadaju.
 
 from copy import deepcopy
-from itertools import pairwise
-from pprint import pprint
-
+from colorama import Fore, Style
 
 Graph = dict[int, set[int]]
 
-N = 4
 
-g = {
-    i: {x for x in range(N) if x != i} for i in range(N)
-}
-
-available = dict[int, set[int]]()
-
-for i in range(N):
-    available[i] = set([x for x in range(N)])
-
-# pprint(g)
-pprint(available)
-
-
-def print_board(row: int, col: int, available: dict[int, set[int]]):
+def print_board(N: int, row: int, col: int, available: Graph):
     str_out = ""
     for i in range(N):
         for j in range(N):
             if i == row and j == col:
-                str_out += ("[Q]")
+                str_out += (Fore.RED + "[Q]" + Style.RESET_ALL)
             elif j in available[i]:
                 str_out += ("[ ]")
             else:
@@ -38,44 +22,41 @@ def print_board(row: int, col: int, available: dict[int, set[int]]):
     print(str_out)
 
 
-def pprint_board(visited_rows_columns: list[tuple[int, int]]):
+def pprint_board(N: int, visited_rows_columns: list[tuple[int, int]]):
     str_out = ""
     for i in range(N):
         for j in range(N):
             if (i, j) in visited_rows_columns:
-                str_out += ("[Q]")
+                str_out += (Fore.RED + "[Q]" + Style.RESET_ALL)
             else:
                 str_out += ("[ ]")
         str_out += ('\n')
     print(str_out)
 
 
-def f(g: Graph, k: int, available: dict[int, set[int]], visited_rows: list[int], visited_columns: list[int]) -> list[tuple[int, int]]:
-    for col in available[k]:
-        cpy = deepcopy(available)
+def solve(N: int, k: int, free_graph: Graph, visited_rows: list[int], visited_columns: list[int]) -> list[tuple[int, int]]:
+    for col in free_graph[k]:
+        graph_copy = deepcopy(free_graph)
         for c in range(0, N):
-            cpy[k].discard(c)
+            graph_copy[k].discard(c)
 
         j = 0
         for row in range(k+1, N):
-
             j += 1
-            cpy[row].discard(col)
+            graph_copy[row].discard(col)
             if col-j >= 0:
-                cpy[row].discard(col-j)
+                graph_copy[row].discard(col-j)
             if col+j < N:
-                cpy[row].discard(col+j)
+                graph_copy[row].discard(col+j)
 
         j = 0
         for row in range(k-1, -1, -1):
             j += 1
-            cpy[row].discard(col)
+            graph_copy[row].discard(col)
             if col-j >= 0:
-                cpy[row].discard(col-j)
+                graph_copy[row].discard(col-j)
             if col+j < N:
-                cpy[row].discard(col+j)
-
-        # forward checking: find MRV
+                graph_copy[row].discard(col+j)
 
         if k not in visited_rows:
             visited_rows.append(k)
@@ -83,37 +64,32 @@ def f(g: Graph, k: int, available: dict[int, set[int]], visited_rows: list[int],
         if col not in visited_columns:
             visited_columns.append(col)
 
+        # Forward checking: find MRV
         min = N+1
         min_key = 0
-        for key in cpy:
-            if key not in visited_rows and len(cpy[key]) < min:
-                min = len(cpy[key])
+        for key in graph_copy:
+            if key not in visited_rows and len(graph_copy[key]) < min:
+                min = len(graph_copy[key])
                 min_key = key
 
-        print_board(k, col, cpy)
+        print_board(N, k, col, graph_copy)
 
         print(list(zip(visited_rows, visited_columns)))
 
         if len(visited_columns) == N:
             return list(zip(visited_rows, visited_columns))
 
-        if len(cpy[min_key]) == 0:
+        if len(graph_copy[min_key]) == 0:
             print("Backtracking\n\n")
             return []
 
-        cpy[k].discard(col)
+        graph_copy[k].discard(col)
 
-        list_of_visited = f(g, min_key, cpy, visited_rows, visited_columns)
+        list_of_visited = solve(N, min_key, graph_copy,
+                                visited_rows, visited_columns)
         if (len(list_of_visited) > 0):
             return list_of_visited
         else:
             visited_rows.pop()
             visited_columns.pop()
     return []
-
-
-res = f(g, 0, available, [], [])
-
-pprint_board(res)
-
-pprint(res)
